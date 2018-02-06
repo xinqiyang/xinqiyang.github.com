a61338d2e2c694b06d9e5154c551dd90926391fe
xinqiyang <xinqiyang@gmail.com>
2018-02-06 18:11:08 +0800

    Site updated: 2018-02-06 18:11:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 06 2018 17:51:42 GMT+0800 (CST)
+// build time:Tue Feb 06 2018 18:11:05 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
