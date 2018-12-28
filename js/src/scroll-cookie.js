3515ac621f26cf8adaa9aa274a9c2615ce2bc35c
yangxinqi <yangxinqi@100intl.co.jp>
2018-12-28 16:33:18 +0900

    Site updated: 2018-12-28 16:33:17

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 03 2018 00:38:05 GMT+0900 (JST)
+// build time:Fri Dec 28 2018 16:33:16 GMT+0900 (JST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
