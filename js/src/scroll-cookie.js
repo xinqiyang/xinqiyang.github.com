d2437de59b08614f94467e0ee4bd6b76d88323e9
xinqiyang <yangxinqi@100intl.co.jp>
2020-01-04 03:23:35 +0900

    Site updated: 2020-01-04 03:23:35

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Jan 04 2020 03:22:28 GMT+0900 (Japan Standard Time)
+// build time:Sat Jan 04 2020 03:23:33 GMT+0900 (Japan Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
